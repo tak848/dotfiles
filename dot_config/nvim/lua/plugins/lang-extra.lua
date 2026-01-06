a96e4ffcb52835851413cf2c9928ea01c1a89e9c
Takuya Takahashi <pairs_char0j@icloud.com>
2026-01-07 08:50:09 +0900

    fix(nvim): use mise LSPs directly + enhance overlook.nvim keymaps (#340)
    ## Summary
    - mise でインストール済みの LSP に `mason = false` を設定して直接使用
      - gopls, rust_analyzer, taplo, jsonnet_ls
    - overlook.nvim のキーマップを拡張
      - `<leader>pU` - restore_all_popups
      - `<leader>pt` - open_in_tab
      - ポップアップ内で `<CR>` / `<C-CR>` でウィンドウ操作
    
    ## Test plan
    - [ ] Go ファイルを開いて gopls が動作する
    - [ ] `<leader>pd` で peek definition が動作する
    - [ ] ポップアップ内で `<CR>` で元ウィンドウに開ける
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- dot_config/nvim/lua/plugins/lang-extra.lua
+++ dot_config/nvim/lua/plugins/lang-extra.lua
@@ -1,28 +1,22 @@
 return {
-    -- TOML (taplo)
+    -- mise でインストール済みの LSP は mason = false で直接使用
     {
         "neovim/nvim-lspconfig",
         opts = {
             servers = {
-                taplo = {},
+                -- mise: go:golang.org/x/tools/gopls
+                gopls = { mason = false },
+                -- mise: aqua:rust-lang/rust-analyzer
+                rust_analyzer = { mason = false },
+                -- mise: aqua:tamasfe/taplo
+                taplo = { mason = false },
+                -- mise: aqua:grafana/jsonnet-language-server
+                jsonnet_ls = { mason = false },
             },
         },
     },
     {
         "nvim-treesitter/nvim-treesitter",
-        opts = { ensure_installed = { "toml" } },
-    },
-    -- jsonnet
-    {
-        "neovim/nvim-lspconfig",
-        opts = {
-            servers = {
-                jsonnet_ls = {},
-            },
-        },
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = { ensure_installed = { "jsonnet" } },
+        opts = { ensure_installed = { "toml", "jsonnet" } },
     },
 }
--- dot_config/nvim/lua/plugins/overlook.lua
+++ dot_config/nvim/lua/plugins/overlook.lua
@@ -5,8 +5,25 @@ return {
         { "<leader>pd", function() require("overlook.api").peek_definition() end, desc = "Peek definition" },
         { "<leader>pc", function() require("overlook.api").close_all() end, desc = "Close all popup" },
         { "<leader>pu", function() require("overlook.api").restore_popup() end, desc = "Restore popup" },
+        { "<leader>pU", function() require("overlook.api").restore_all_popups() end, desc = "Restore all popups" },
         { "<leader>ps", function() require("overlook.api").open_in_split() end, desc = "Open in split" },
         { "<leader>pv", function() require("overlook.api").open_in_vsplit() end, desc = "Open in vsplit" },
+        { "<leader>pt", function() require("overlook.api").open_in_tab() end, desc = "Open in tab" },
         { "<leader>po", function() require("overlook.api").open_in_original_window() end, desc = "Open in window" },
     },
+    init = function()
+        vim.api.nvim_create_autocmd("BufWinEnter", {
+            callback = function()
+                local dominated = require("overlook.api").is_dominated_win()
+                if dominated then
+                    vim.keymap.set("n", "<CR>", function()
+                        require("overlook.api").open_in_original_window()
+                    end, { buffer = true, desc = "Open in window" })
+                    vim.keymap.set("n", "<C-CR>", function()
+                        require("overlook.api").open_in_split()
+                    end, { buffer = true, desc = "Open in split" })
+                end
+            end,
+        })
+    end,
 }
