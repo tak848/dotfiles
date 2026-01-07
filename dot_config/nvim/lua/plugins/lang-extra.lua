ba95021670d04d52ee9e6468cfe8ad33038a85a2
TAK848 <pairs_char0j@icloud.com>
2026-01-07 09:27:28 +0900

    fix(nvim): deprecated API を修正、servers['*'].keys で gd を設定

--- dot_config/nvim/lua/plugins/lang-extra.lua
+++ dot_config/nvim/lua/plugins/lang-extra.lua
@@ -3,28 +3,24 @@ return {
     -- gd を overlook の peek_definition に置き換え
     {
         "neovim/nvim-lspconfig",
-        opts = function(_, opts)
-            opts.servers = opts.servers or {}
-            -- mise: go:golang.org/x/tools/gopls
-            opts.servers.gopls = { mason = false }
-            -- mise: aqua:rust-lang/rust-analyzer
-            opts.servers.rust_analyzer = { mason = false }
-            -- mise: aqua:tamasfe/taplo
-            opts.servers.taplo = { mason = false }
-            -- mise: aqua:grafana/jsonnet-language-server
-            opts.servers.jsonnet_ls = { mason = false }
-
-            -- LSP キーマップで gd を overlook に置き換え
-            local keys = require("lazyvim.plugins.lsp.keymaps").get()
-            keys[#keys + 1] = {
-                "gd",
-                function()
-                    require("overlook.api").peek_definition()
-                end,
-                desc = "Goto Definition",
-                has = "definition",
-            }
-        end,
+        opts = {
+            servers = {
+                -- 全 LSP 共通: gd を overlook に置き換え
+                ["*"] = {
+                    keys = {
+                        { "gd", function() require("overlook.api").peek_definition() end, desc = "Goto Definition", has = "definition" },
+                    },
+                },
+                -- mise: go:golang.org/x/tools/gopls
+                gopls = { mason = false },
+                -- mise: aqua:rust-lang/rust-analyzer
+                rust_analyzer = { mason = false },
+                -- mise: aqua:tamasfe/taplo
+                taplo = { mason = false },
+                -- mise: aqua:grafana/jsonnet-language-server
+                jsonnet_ls = { mason = false },
+            },
+        },
     },
     {
         "nvim-treesitter/nvim-treesitter",
